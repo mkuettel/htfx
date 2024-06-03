85704318dc2489d83c76c69c8bf2ec249ddcce04
Moritz Küttel <moritz_kuettel+htfx_gitlog@fastmail.com>
2024-06-03 17:49:36 +0200

    tests(UnitTester): fix requireTemplate

--- tests/Support/UnitTester.php
+++ tests/Support/UnitTester.php
@@ -29,7 +29,7 @@ class UnitTester extends \Codeception\Actor
 
     public function requireTemplate(string $name)
     {
-        $file = __DIR__ . '/Helper/' . $namene . '.php';
+        $file = __DIR__ . '/Helper/' . $name . '.php';
         return (require $file);
     }
 
